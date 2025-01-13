04c8d4d997bfb041716adea425e215b0af11f56d
Ritikraj11 <160333960+Ritikraj11@users.noreply.github.com>
2025-01-13 22:31:06 +0530

    basic InputOutput and Basic operators

---
+++ InputOutput.cpp
@@ -0,0 +1,49 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int age =25;
+//     cout << age ;
+//     return 0;
+// }
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     char grade = 'A';
+//     int value = grade;
+//     cout << value << endl;
+//     return 0;
+// }
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     double price ;
+//     cout << "Enter the Price : " << endl;
+//     cin >> price ;
+//     cout<< "The price is = " << price <<endl;
+//     return 0;
+// }
+
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 15 , b =6;
+
+    int sum = a+b ;
+    int difference = a-b ;
+    int multiply = a*b ;
+    int divide = a/b ;
+    int modulo = a%b;
+
+    cout<< "The Sum is : " << sum <<endl;
+    cout<< "The difference is : " << difference <<endl;
+    cout<< "The multiply is : " << multiply <<endl;
+    cout<< "The divide is : " << divide <<endl;
+    cout<< "The remainder is : " << modulo <<endl;
+
+
+    return 0;
+}
---
+++ operators.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 15 , b =6;
+
+    int sum = a+b ;
+    int difference = a-b ;
+    int multiply = a*b ;
+    int divide = a/b ;
+    int modulo = a%b;
+
+    cout<< "The Sum is : " << sum <<endl;
+    cout<< "The difference is : " << difference <<endl;
+    cout<< "The multiply is : " << multiply <<endl;
+    cout<< "The divide is : " << divide <<endl;
+    cout<< "The remainder is : " << modulo <<endl;
+
+
+    return 0;
+}
