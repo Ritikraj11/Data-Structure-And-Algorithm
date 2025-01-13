10c45550d47a4f13b5318075ee845da0b0bba07c
Ritikraj11 <160333960+Ritikraj11@users.noreply.github.com>
2025-01-13 22:56:16 +0530

    made a seperate file for operators

--- operators.cpp
+++ operators.cpp
@@ -1,19 +1,32 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int a = 15 , b =6;
+    // int a = 15 , b =6;
 
-    int sum = a+b ;
-    int difference = a-b ;
-    int multiply = a*b ;
-    int divide = a/b ;
-    int modulo = a%b;
+    // int sum = a+b ;
+    // int difference = a-b ;
+    // int multiply = a*b ;
+    // int divide = a/b ;
+    // int modulo = a%b;
 
-    cout<< "The Sum is : " << sum <<endl;
-    cout<< "The difference is : " << difference <<endl;
-    cout<< "The multiply is : " << multiply <<endl;
-    cout<< "The divide is : " << divide <<endl;
-    cout<< "The remainder is : " << modulo <<endl;
+    // cout<< "The Sum is : " << sum <<endl;
+    // cout<< "The difference is : " << difference <<endl;
+    // cout<< "The multiply is : " << multiply <<endl;
+    // cout<< "The divide is : " << divide <<endl;
+    // cout<< "The remainder is : " << modulo <<endl;
+
+
+    // cout << (5 / 2) << endl ;    // Output is 2 because both the values are integer type
+    cout << (5/(double(2))) << endl ; // Now one of the value is not integer so the value is 2.5 and the process is known as typecasting
+    
+
+
+
+
+
+
+
+
 
 
     return 0;
