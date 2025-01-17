4be13998490ac9c72183639120fe16477558175f
Ritikraj11 <160333960+Ritikraj11@users.noreply.github.com>
2025-01-17 12:34:34 +0530

    While Loop, For loop and some Problems

--- Problems.cpp
+++ Problems.cpp
@@ -14,16 +14,71 @@
 
 // Q2. TO check the Input character is uppercase or lowercase
 
-    #include <iostream>
-    using namespace std;
-    int main(){
-        char ch;
-        cout<< "Enter The character : " << endl;
-        cin>>ch;
-        if(ch >= 'a' && ch<= 'z'){
-            cout << "The Character is LowerCase" << endl;
-        } else {
-            cout << "The Character is UpperCase" << endl;
+    // #include <iostream>
+    // using namespace std;
+    // int main(){
+    //     char ch;
+    //     cout<< "Enter The character : " << endl;
+    //     cin>>ch;
+    //     if(ch >= 'a' && ch<= 'z'){
+    //         cout << "The Character is LowerCase" << endl;
+    //     } else {
+    //         cout << "The Character is UpperCase" << endl;
+    //     }
+    //     return 0;
+    // }
+
+// Q3 . Sum of number from 1 to 
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int sum=0, i;
+//     int n;
+//     cout << "Enter the Value of Number till which You Want Sum : ";
+//     cin >> n;
+//     for(i =1 ;i<=n ; i++){
+//         sum =sum+i;
+
+//     }
+//     cout <<"sum: " << sum ;
+// } 
+
+
+            // #include<iostream>
+            // using namespace std;
+            // int main(){
+            //     int sum=0, i;
+            //     int n;
+            //     cout << "Enter the Value of Number till which You Want Sum : ";
+            //     cin >> n;
+            //     for(i =1 ;i<=n ; i++){
+            //         sum =sum+i;
+            //         if(i==4){  // Due to this 'i' will run only 4 times
+            //             break;
+            //         }
+            //     }
+            //     cout <<"sum: " << sum ;
+            // } 
+
+// Q4. To sum of all odd numbers from 1 to n
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n = 50;
+    cout << "Odd Numbers are : ";
+    for (int i = 1 ; i<=n; i++){
+        
+        if(i%2 !=0){
+            
+            cout << i <<" ";
         }
-        return 0;
+
     }
+
+return 0;
+}
+
+
---
+++ loops.cpp
@@ -0,0 +1,40 @@
+// 1. While Loop
+
+// Syntex :: while (condition){
+
+// update;
+// }
+
+// 1. To print 1 to 5 
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int count=1;
+//     while(count<=5){
+//         cout << count << " ";
+//         count++;
+//     }
+//     return 0;
+// }
+
+
+// ************* FOR LOOP **********************************
+// Syntex : for(initialisation; condition; updation){
+//     block of code
+// }
+
+// 2. To print 1 to 10 with for loop
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n=10;
+    for(int i=1 ; i<=n ; i++)
+    {
+        cout << i << " ";
+        }
+        return 0;
+}
